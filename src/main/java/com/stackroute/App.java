c34fd7a4375ec6b003d648c2d3c10188d256dcec
Rachan <rachkau1@in.ibm.com>
2019-01-09 15:50:26 +0530

    learnt and coded dependency injection

---
+++ src/main/java/com/stackroute/App.java
@@ -0,0 +1,18 @@
+package com.stackroute;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        ApplicationContext context = new ClassPathXmlApplicationContext("triangle.xml");
+        Triangle triangle =(Triangle) context.getBean("triangle");
+        triangle.draw();
+    }
+}
